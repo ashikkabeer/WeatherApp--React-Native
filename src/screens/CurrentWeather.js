3d39c93d99c48847859377d8a737d0f1e9a07898
Ashik Kabeer <ashikkabeer17@gmail.com>
2023-08-21 13:40:50 +0530

    Initial commit

---
+++ src/screens/CurrentWeather.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native'
+import {Feather} from '@expo/vector-icons';
+import RowText from '../components/RowText'
+import { weatherType } from '../utilities/weatherType'
+
+const CurrentWeather = ({ weatherData }) => {
+    const { main: { temp, feels_like, temp_max, temp_min }, weather } = weatherData
+    const weatherCondition = weather[0].main
+
+    const {wrapper, container, temperature, deels, hiLowWrapper, hiLow, bodyWrapper, description, message} = styles
+    
+    return(
+        <SafeAreaView 
+            style={[wrapper,{backgroundColor:weatherType[weatherCondition]?.backgroundColor}
+            ]}
+        >
+            <View style={container}>
+                <Feather
+                name={weatherType[weatherCondition]?.icon}
+                size = {100}
+                color = "white"
+                />
+                <Text style={temperature}>{`${temp}°`}</Text>
+                <RowText
+                    messageOne = {`High: ${temp_max}`}
+                    messageTwo={`Low:${temp_min}`}
+                    containerStyles = {hiLowWrapper}
+                    messageOneStyles = {hiLow}
+                    messageTwoStyles = {hiLow}
+                />
+            </View>
+            <RowText
+                messageOne={weather[0]?.description}
+                messageTwo={weatherType[weatherCondition]?.message}
+                containerStyles={bodyWrapper}
+                messageOneStyles={description}
+                messageTwoStyles={message}
+            />
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    wrapper:{
+        flex : 1 ,
+    },
+    container:{
+        color: 'black',
+        fontSize: 48,
+    },
+    feels: {
+        fontSize:30,
+
+    },
+    hiLow: {
+        color:'black',
+        fontSize: 20
+    },
+    hiLowWrapper: {
+        flexDirection:'row',
+    },
+    bodyWrapper:{
+        justifyContent:"space-end",
+        alignItems: 'flex-start',
+        paddingLeft: 25,
+        marginBottom: 40
+    },
+    description: {
+        fontSize: 43
+      },
+      message: {
+        fontSize: 25
+      }
+})
+
+export default  CurrentWeather;
